621c7a04496d0197aa4fb17a964a996c287ca96e
Karume-lab <danielkarume@outlook.com>
2023-09-05 13:43:59 +0300

    styled the features section

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// document.onreadystatechange = function() {
-// 	var loadingAnimation = document.getElementById("loading");
-// 	var pageContent =  document.getElementById('pageContent');
+document.onreadystatechange = function() {
+	var loadingAnimation = document.getElementById("loading");
+	var pageContent =  document.getElementById('pageContent');
 
-// 	setTimeout(function(){
-// 		if (document.readyState === "complete") {
-// 			loadingAnimation.classList.add('hidden');
-// 			pageContent.classList.remove('hidden');
-// 		};
-// 	}, 2000)
-// };
+	setTimeout(function(){
+		if (document.readyState === "complete") {
+			loadingAnimation.classList.add('hidden');
+			pageContent.classList.remove('hidden');
+		};
+	}, 2000)
+};
