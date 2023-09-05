9c530a990553a6d04592389cfd8f8a3a8c87ee39
Karume-lab <danielkarume@outlook.com>
2023-09-05 13:56:11 +0300

    fixedelemets showing while page is still loading

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.onreadystatechange = function() {
 	var loadingAnimation = document.getElementById("loading");
 	var pageContent =  document.getElementById("pageContent");
-	var logo = document.getElementById("logo");
+
 
 	setTimeout(function(){
 		if (document.readyState === "complete") {
 			loadingAnimation.classList.add("hidden");
 			pageContent.classList.remove("hidden");
-			logo.classList.remove("hidden");
 		};
 	}, 2000)
 };
