3391d4e20b95c87e3593f07d87b8165f86df57b1
Karume-lab <danielkarume@outlook.com>
2023-09-05 13:48:43 +0300

    fixed logo showing while page is still loading

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 document.onreadystatechange = function() {
 	var loadingAnimation = document.getElementById("loading");
-	var pageContent =  document.getElementById('pageContent');
+	var pageContent =  document.getElementById("pageContent");
+	var logo = document.getElementById("logo");
 
 	setTimeout(function(){
 		if (document.readyState === "complete") {
-			loadingAnimation.classList.add('hidden');
-			pageContent.classList.remove('hidden');
+			loadingAnimation.classList.add("hidden");
+			pageContent.classList.remove("hidden");
+			logo.classList.remove("hidden");
 		};
 	}, 2000)
 };
